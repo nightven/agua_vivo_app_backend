05c50c53be3ff3a67ee122b8e15fc4a70b3014d2
Виталий Беяр <vitaliy.beyar@gmail.com>
2024-01-24 19:13:03 -0330

    Add validate shemas for Water, JOI and Mangoose

--- schemas/waterShemas.js
+++ schemas/waterShemas.js
@@ -1,5 +1,33 @@
 const Joi = require("joi");
 
-const waterShema = Joi.object({});
+const { Schema, model } = require("mongoose");
 
-module.exports = { waterShema };
+const handleMongooseError = require("../helpers/handleMongooseError");
+
+
+const waterRateSchema = Joi.object({
+  waterRate: Joi.number()
+    .min(1)
+    .max(15000)
+    .required()
+    .message({ "any.required": "missing required waterRate field" }),
+});
+
+const waterNotes = Joi.object({
+  waterVolume: Joi.number()
+    .min(1)
+    .max(15000)
+    .required()
+    .message({ "any.required": "missing required waterVolume field" }),
+
+  date: Joi.date()
+    .required()
+    .message({ "any.required": "missing required date field" }),
+});
+
+const schemas = {
+  waterRateSchema,
+  waterNotes,
+};
+
+module.exports = { schemas };
