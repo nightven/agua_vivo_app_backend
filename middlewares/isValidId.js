039876420579e083c71c930c3174a67226d841d5
Виталий Беяр <vitaliy.beyar@gmail.com>
2024-01-24 22:30:16 -0330

    added HttError method and isValidId method

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -1,5 +1,12 @@
 const { isValidObjectId } = require("mongoose");
 
-const isValidId = (req, res, next) => {};
+const isValidId = (req, res, next) => {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) {
+    next(httpError(400, `${id} is not a valid id`));
+  }
+  next();
+};
 
 module.exports = isValidId;
